db65c38abf9ad8759cbf5565f09ef1474a5d2014
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-18 14:57:40 +0500

    complete NavBar component design
    and add images folder in public

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,35 @@
+import styles from "../styles/NavBar.module.css"
+import Image from "next/image"
+
 function NavBar() {
   return (
-    <div>
-      NavBar
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src={"/images/telephone.png"} width={32} height={32} alt="" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>0315 8604063</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+            <li className={styles.listItem}>Homepage</li>
+            <li className={styles.listItem}>Products</li>
+            <li className={styles.listItem}>Menu</li>
+            <Image src="/images/logo.png" alt="" width="160px" height="69px" />
+            <li className={styles.listItem}>Events</li>
+            <li className={styles.listItem}>Blog</li>
+            <li className={styles.listItem}>Contact</li>
+          </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/images/cart.png" alt="" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   )
 }
