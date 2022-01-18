108fc94fcf50c0a2c5f17f177157d0e2fb4d75c5
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-18 20:08:02 +0500

    product listing completed
    added two component i,e PizzaList and PizzaCard and complete design of product listing

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Slider from '../components/Slider'
+import PizzaList from '../components/PizzaList'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Next JS Food Ordering App by Shahid</title>
+        <title>NEXT JS Food Ordering App by Shahid</title>
         <meta name="description" content="Best pizza shop in Karachi." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Slider />
+      <PizzaList />
     </div>
   )
 }
