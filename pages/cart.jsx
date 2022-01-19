f412752cdbd5493a13cc7145dcae4a949c1c247c
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-19 16:56:04 +0500

    complete design of cart page
    cart page right side completed

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -42,11 +42,54 @@ function Cart() {
               <span className={styles.total}>$39.80</span>
             </td>
           </tr>
+          <tr className={styles.tr}>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image 
+                  src={"/images/pizza.png"} 
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce	
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$19.90</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$39.80</span>
+            </td>
+          </tr>
         </table>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Cart
+
