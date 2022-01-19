d52812dcbfa5048657d00ccbff526cf53d67892f
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-19 15:34:27 +0500

    product page completed
    product page right side completed and don product page design

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,7 +1,10 @@
 import styles from "../../styles/Product.module.css"
 import Image from "next/image"
+import { useState } from "react";
 
 function Product() {
+  const [size, setSize] = useState(0)
+  
   const pizza = {
     id: 1,
     img: "/images/pizza.png",
@@ -17,7 +20,73 @@ function Product() {
           <Image src={pizza.img} layout="fill" objectFit="contain" />
         </div>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className={styles.price}>${pizza.price[size]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose the size</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size} onClick={() => setSize(0)}>
+            <Image src={"/images/size.png"} layout="fill" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(1)}>
+            <Image src={"/images/size.png"} layout="fill" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(2)}>
+            <Image src={"/images/size.png"} layout="fill" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+        <h3 className={styles.choose}>Choose additional ingredients</h3>
+        <div className={styles.ingredients}>
+          <div className={styles.option}>
+            <input 
+              className={styles.checkbox}
+              type="checkbox" 
+              id="double" 
+              name="double" 
+            />
+            <label htmlFor="double">Double Ingredients</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              id="cheese"
+              name="cheese"
+            />
+            <label htmlFor="cheese">Extra Cheese</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              id="spicy"
+              name="spicy"
+            />
+            <label htmlFor="spicy">Spicy Sauce</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              id="garlic"
+              name="garlic"
+            />
+            <label htmlFor="garlic">Garlic Sauce</label>
+          </div>
+        </div>
+        <div className={styles.add}>
+          <input
+            className={styles.quantity}
+            type="number"
+            defaultValue={1}
+          />
+          <button className={styles.button}>Add to Cart</button>
+        </div>
+      </div>
     </div>
   )
 }
