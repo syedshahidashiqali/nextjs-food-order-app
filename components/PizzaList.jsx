c4b8f910c023a4b94ab1d1f510126ce4f825bba3
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-22 00:01:37 +0500

    fetch dynamically data of pizza and display them
    add getServerSideProps func for fetching data of pizza list and show pass them via prop to Home page and then passed from Home page to PizzaList and Pizaa

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/PizzaList.module.css"
 import PizzaCard from "./PizzaCard"
 
-function PizzaList() {
+function PizzaList({ pizzaList }) {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
@@ -11,14 +11,9 @@ function PizzaList() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
       <div className={styles.wrapper}>
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
-        <PizzaCard />
+        {pizzaList.map(pizza => (
+          <PizzaCard pizza={pizza} key={pizza._id} />
+        ))}
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Slider from '../components/Slider'
 import PizzaList from '../components/PizzaList'
+import axios from "axios"
 
-export default function Home() {
+export default function Home({ pizzaList }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +13,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Slider />
-      <PizzaList />
+      <PizzaList pizzaList={pizzaList} />
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  
+  const res = await axios.get("http://localhost:3000/api/products")
+
+  return {
+    props: {
+      pizzaList: res.data,
+    }
+  }
 }
