5ba2e41fb7034c80ae24302bbe6d5ede8d763349
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-23 18:36:31 +0500

    update cart.jsx and cartSlice.js
    done functionality for order done and when it is successful payment we redirect the user to that order page

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -7,13 +7,27 @@ import {
     PayPalButtons,
     usePayPalScriptReducer
 } from "@paypal/react-paypal-js";
+import axios from "axios"
+import { useRouter } from "next/router"
+import { reset } from "../redux/cartSlice"
 
 function Cart() {
   const dispatch = useDispatch()
   const cart = useSelector(state => state.cart)
+  const router = useRouter()
+
+  const createOrder = async (data) => {
+    try{
+      const res = await axios.post("http://localhost:3000/api/orders", data)
+      res.status === 201 && router.push(`/orders/${res.data._id}`)
+      dispatch(reset())
+    } catch(err){
+      console.log(err)
+    }
+  }
   const [open, setOpen] = useState(false)
   // This values are the props in the UI
-  const amount = "2";
+  const amount = cart.total;
   const currency = "USD";
   const style = { "layout": "vertical" };
 
@@ -60,8 +74,15 @@ function Cart() {
               });
           }}
           onApprove={function (data, actions) {
-            return actions.order.capture().then(function () {
+            return actions.order.capture().then(function (details) {
               // Your code here after capture the order
+              const shipping = details.purchase_units[0].shipping
+              createOrder({
+                customer: shipping.name.full_name,
+                address: shipping.address.address_line_1,
+                total: cart.total,
+                method: 1,
+              })
             });
           }}
         />
