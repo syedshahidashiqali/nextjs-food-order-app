5e8dcf4aeba9dfe28c92b9a5ea78cf57d16ba175
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-24 18:26:49 +0500

    add and complete two components
    Add for Modal for adding product and AddButton that shows and hides that Modal

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import styles from '../styles/Home.module.css'
 import Slider from '../components/Slider'
 import PizzaList from '../components/PizzaList'
 import axios from "axios"
+import { useState } from "react"
+import AddButton from '../components/AddButton'
+import Add from '../components/Add'
 
-export default function Home({ pizzaList }) {
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true)
   return (
     <div className={styles.container}>
       <Head>
@@ -13,18 +17,27 @@ export default function Home({ pizzaList }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Slider />
+      {admin && <AddButton setClose={setClose} />}
       <PizzaList pizzaList={pizzaList} />
+      {!close && <Add setClose={setClose} />}
     </div>
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || ""
+  let admin = false
+
+  if(myCookie.token === process.env.TOKEN) {
+    admin = true
+  }
   
   const res = await axios.get("http://localhost:3000/api/products")
 
   return {
     props: {
       pizzaList: res.data,
+      admin,
     }
   }
 }
