5a7617c958fd53055a3c21f7124ce170c9687986
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-24 14:42:25 +0500

    Update login.js
    API for login admin complete

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,24 @@
-import dbConnect from "../../../util/dbConnect"
+import cookie from "cookie"
 
-export default async function handler(req, res) {
-  dbConnect()
+export default function handler(req, res) {
 
-  if(method === "GET"){}
+  if(method === "POST"){
+
+    const { username, password } = req.body
+    if(username === process.env.USERNAME && password === process.env.PASSWORD) {
+      res.setHeaders(
+        "Set-Cookie",
+        cookie.serialize("token", process.env.TOKEN, {
+          maxAge: 60 * 60, // number in seconds, it is one hour
+          sameSite: "strict",
+          path: "/",
+          // httpOnly: true,
+          // secure: true,
+        })
+      )
+    }
+    res.status(200).json("Successful!")
+  } else {
+    res.status(400).json("Wrong credentials!")
+  }
 }
