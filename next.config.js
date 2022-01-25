9a1f94c181fea30c17e8d8203b3cda78f33eda09
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-25 12:27:12 +0500

    testing

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://nextjs-food-order-app-1jo18d5ha-syedshahidashiqali.vercel.app/:path*',
+      },
+    ]
+  },
 }
