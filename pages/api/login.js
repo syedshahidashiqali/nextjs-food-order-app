5f87ccdaea85984d0d8b533024803487ecfd0e14
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-26 12:35:28 +0500

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         "Set-Cookie",
         cookie.serialize("token", process.env.TOKEN, {
           maxAge: 60 * 60, // number in seconds, it is one hour
-          // sameSite: "strict",
+          sameSite: "strict",
           path: "/",
           // httpOnly: true,
           // secure: true,
