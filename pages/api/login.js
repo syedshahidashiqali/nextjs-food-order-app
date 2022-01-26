e6977acfd80e2e4c810706a43e5dbca806f04b82
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-26 14:35:51 +0500

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,27 +1,27 @@
 import cookie from "cookie"
-import NextCors from 'nextjs-cors';
+// import NextCors from 'nextjs-cors';
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
 
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200,
-  });
+  // await NextCors(req, res, {
+  //   // Options
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   origin: '*',
+  //   optionsSuccessStatus: 200,
+  // });
 
   if(req.method === "POST"){
 
     const { username, password } = req.body
     if(username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
-      await res.setHeader(
+      res.setHeader(
         "Set-Cookie",
         cookie.serialize("token", process.env.TOKEN, {
           maxAge: 60 * 60, // number in seconds, it is one hour
-          // sameSite: "strict",
+          sameSite: "strict",
           path: "/",
           // httpOnly: true,
-          secure: true,
+          // secure: true,
         })
       )
       res.status(200).json("Successful!")
